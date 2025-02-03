a191a490ad46e52690c19bfde5ee1173422d139a
Raúl Ignacio Vergara Leiva <raul.vergara1@mail.udp.cl>
2025-02-02 22:23:26 -0300

    correccion del tailwind css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    './app/**/*.{js,ts,jsx,tsx}', // Archivos en la carpeta app/
+    './components/**/*.{js,ts,jsx,tsx}', // Archivos en la carpeta components/
+    './pages/**/*.{js,ts,jsx,tsx}', // Archivos en la carpeta pages/ (si existe)
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
-
+};
